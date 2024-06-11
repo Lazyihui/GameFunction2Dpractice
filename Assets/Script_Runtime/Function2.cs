0f5ba728d3c439d84b309891c697ee9d95d12bf5
包子 <chengbaozi6@qq.com>
2024-06-11 22:14:46 +0800

    <WIP>重复添加

--- Assets/Script_Runtime/Function2.cs
+++ Assets/Script_Runtime/Function2.cs
@@ -16,13 +16,70 @@ public class Function2 {
             new Vector2Int(0, -1)
         };
 
+    public Vector2Int[] hinder = new Vector2Int[]{
+        new Vector2Int(6,8),
+        new Vector2Int(6,9),
+        new Vector2Int(6,10)
+    };
 
-    // 添加一开始的位置
-    public void Init(Vector2Int start, Vector2Int end) {
+    public int ProcessCellMain(Vector2Int start, Vector2Int end,/*不能走的位置先手输*/Vector2Int[] hinders) {
+
+        closeSetKey.Clear();
+        for (int i = 0; i < 3; i++) {
+            Vector2Int hinder = hinders[i];
+            RectCell2 tem = new RectCell2();
+            tem.Init(hinder, 10000, 10000, 1000000);
+            closeSetKey.Add(hinder, tem);
+        }
+        // 添加一开始的位置
         RectCell2 startCell = new RectCell2();
         startCell.Init(start, 0, 0, 0);
         openSetKey.Add(start, startCell);
+
+        AddNeighbor(startCell, end);
+
+        RectCell2 nextCell = new RectCell2();
+
+        nextCell = GetMinFCell(end);
+
+        openSetKey.Add(nextCell.position, nextCell);
+        if (nextCell.position == end) {
+            // 结束这个函数
+
+            return -1;
+
+        } else {
+            return 0;
+        }
+
     }
+
+
+
+    public int ProcessCell(Vector2Int start, Vector2Int end,/*不能走的位置先手输*/Vector2Int[] hinders) {
+
+        // 添加一开始的位置
+        RectCell2 startCell = new RectCell2();
+        startCell.Init(start, 0, 0, 0);
+
+        AddNeighbor(startCell, end);
+
+        RectCell2 nextCell = new RectCell2();
+
+        nextCell = GetMinFCell(end);
+
+        openSetKey.Add(nextCell.position, nextCell);
+        if (nextCell.position == end) {
+            // 结束这个函数
+
+            return -1;
+
+        } else {
+            return 0;
+        }
+
+    }
+
 
     public void AddNeighbor(RectCell2 start_Cur, Vector2Int end) {
 
@@ -33,11 +90,18 @@ public class Function2 {
             RectCell2 tem = new RectCell2();
             tem.position = neighborPos;
             tem.hCost = 10;
-            tem.gCost = H_Manhattan(neighborPos,end);
-            tem.fCost = 0;
+            tem.gCost = H_Manhattan(neighborPos, end);
+            tem.fCost = tem.hCost + tem.gCost;
+
+            if (neighborPos == end) {
+                Debug.Log("走到了");
+            }
+
             if (closeSetKey.ContainsKey(neighborPos)) {
+                Debug.Log("已经存在");
 
             } else {
+                Debug.Log(tem.fCost);
                 closeSetKey.Add(neighborPos, tem);
             }
         }
@@ -49,9 +113,11 @@ public class Function2 {
         RectCell2 minCell = null;
         // 查找所有的值 找到最小的
         foreach (var cell in closeSetKey.Values) {
-            Debug.Log(closeSetKey.Values);
+
+            // Debug.Log(closeSetKey.Values);
 
             if (minCell == null || cell.fCost < minCell.fCost) {
+
                 minCell = cell;
             }
 
--- Assets/Script_Runtime/Main.cs
+++ Assets/Script_Runtime/Main.cs
@@ -3,58 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
-    // Start is called before the first frame update
 
-    HashSet<Vector2Int> blockSet = new HashSet<Vector2Int>();
-    Vector2Int startPos;
-    Vector2Int endPos;
-    Vector2Int curPos;
-
-    Vector2Int[] result = new Vector2Int[10000];
-    int resultCount = 0;
-
-    int visited = 0;
-
-    Function function;
-
+    Function2 function2;
+    int a;
     void Awake() {
-        Debug.Log("Main Awake");
 
-        function = new Function();
-
-
+        function2 = new Function2();
+        function2.openSetKey.Clear();
+        a = function2.ProcessCellMain(new Vector2Int(5, 9), new Vector2Int(8, 7), function2.hinder);
     }
 
-    // Update is called once per frame
     void Update() {
 
 
-        Vector2Int mouseGridPos = MouseGridPos();
-        if (Input.GetMouseButton(0)) {
-            blockSet.Add(mouseGridPos);
-        } else if (Input.GetMouseButtonUp(1)) {
-            startPos = mouseGridPos;
-        } else if (Input.GetMouseButtonUp(2)) {
-            endPos = mouseGridPos;
-            visited = 0;
-            resultCount = function.Astar(startPos, endPos, 5000, (pos) => {
-                return !blockSet.Contains(pos);
-            }, result);
-        } else if (Input.GetKeyDown(KeyCode.Space)) {
-            bool hasResult = function.ProcessCell(ref visited, ref resultCount, 5000, startPos, endPos, (pos) => {
-                return !blockSet.Contains(pos);
-            }, result, out curPos);
-            if (hasResult) {
-                Debug.Log("Res: " + resultCount);
-            }
+
+        if (a == 0) {
+            a = function2.ProcessCell(new Vector2Int(5, 9), new Vector2Int(8, 7), function2.hinder);
+        } else {
+
         }
 
-    }
-    Vector2Int MouseGridPos() {
-        Vector2 mouseScreenPos = Input.mousePosition;
-        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-        Vector2Int mouseGridPos = new Vector2Int(Mathf.RoundToInt(mouseWorldPos.x), Mathf.RoundToInt(mouseWorldPos.y));
-        return mouseGridPos;
+
+
     }
 
 
